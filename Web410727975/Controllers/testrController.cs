53b31fa02a1c6c040acd539b20f1b16a55ecb423
黃書韓 <s1072797@gm.pu.edu.tw>
2021-05-06 18:38:02 +0800

    上次的testcontroller

---
+++ Web410727975/Controllers/testrController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410727975.Controllers
+{
+    public class testrController : Controller
+    {
+        // GET: testr
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
